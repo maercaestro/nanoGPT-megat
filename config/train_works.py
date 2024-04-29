b26ca6d9bdc0881b56339243bb1b3c0bcedfef11
maercaestro <maercaestro@gmail.com>
2024-04-29 13:48:23 +0800

    Update train_works.py

--- config/train_works.py
+++ config/train_works.py
@@ -15,8 +15,8 @@ wandb_run_name = 'mini-gpt'
 
 dataset = 'abuworks'
 gradient_accumulation_steps = 1
-batch_size = 32
-block_size = 512 # context of up to 256 previous characters
+batch_size = 16
+block_size = 1024 # context of up to 256 previous characters
 
 # baby GPT model :)
 n_layer = 4
@@ -25,7 +25,7 @@ n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 1000
+max_iters = 2000
 lr_decay_iters = 5000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
